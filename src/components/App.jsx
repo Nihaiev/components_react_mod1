3ad1a9abdb71ef716c49bd98af9de769a7857039
Ivan <nigaev.ivan@gmail.com>
2022-10-09 17:01:43 +0300

    add components

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { PageTitle } from 'components/PageTitle/PageTitle';
+
+export const App = () => {
+     return (
+        <div>
+            <PageTitle text="24th Core World" />
+        </div>
+    );
+};
+
+//1:14
