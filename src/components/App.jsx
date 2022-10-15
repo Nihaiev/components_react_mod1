66d8f3b6d5bda403dff88fc83b819476523d7b69
Ivan <nigaev.ivan@gmail.com>
2022-10-15 18:47:14 +0300

    new project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,3 @@
-import { PageTitle } from 'components/PageTitle/PageTitle';
-
 export const App = () => {
-     return (
-        <div>
-            <PageTitle text="24th Core World" />
-        </div>
-    );
+  return <div>App</div>
 };
-
-//1:20
